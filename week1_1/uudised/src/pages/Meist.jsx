1231e7b5d2f86b8948ed4ca25746224b87d2e014
Mark Hallap <hallapmark@gmail.com>
2025-10-25 19:42:59 +0300

    Lisa töötajate kontakti dünaamiline väljakuvamine

--- week1_1/uudised/src/pages/Meist.jsx
+++ week1_1/uudised/src/pages/Meist.jsx
@@ -1,26 +1,46 @@
+import { useState } from "react";
+
 function Meist() {
+  const [kontakt, setKontakt] = useState("");
+
+  function n2itaKontakt(e, kontakt) {
+    e.stopPropagation();
+    setKontakt(kontakt);
+  }
+
   return (
-    <div>
-       <div>See on meist leht, nähtav localhost:5173/meist aadressil</div>
-       <div>Meie töötajad:</div>
-       <br />
-       <div>Arvo Pärt</div>
-       <div>+372 123123</div>
-       <div>Uudisklippide taustamuusika</div>
-       <br />
-       <div>Kelly Sildaru</div>
-       <div>+372 123124</div>
-       <div>Püstolreporter</div>
-       <br />
-       <div>Edward von Lõngus</div>
-       <div>+372 123125</div>
-       <div>Uudiste kujundamine</div>
-       <br />
-       <div>Kerli Kõiv</div>
-       <div>+372 123126</div>
-       <div>Välisturgude spetsialist</div>
+    <div onClick={() => setKontakt("")}>
+      <div>See on meist leht, nähtav localhost:5173/meist aadressil</div>
+      <div>Meie töötajad:</div>
+      <br />
+
+      <div>Arvo Pärt</div>
+      <div>Uudisklippide taustamuusika</div>
+      <button onClick={(e) => n2itaKontakt(e, "+372 123123")}>Võta ühendust</button>
+      <br />
+      <br />
+
+      <div>Kelly Sildaru</div>
+      <div>Püstolreporter</div>
+      <button onClick={(e) => n2itaKontakt(e, "+372 123124")}>Võta ühendust</button>
+      <br />
+      <br />
+
+      <div>Edward von Lõngus</div>
+      <div>Uudiste kujundamine</div>
+      <button onClick={(e) => n2itaKontakt(e, "+372 123125")}>Võta ühendust</button>
+      <br />
+      <br />
+
+      <div>Kerli Kõiv</div>
+      <div>Välisturgude spetsialist</div>
+      <button onClick={(e) => n2itaKontakt(e, "+372 123126")}>Võta ühendust</button>
+      <br />
+      <br />
+
+      {kontakt !== "" && <div>Tema kontakt: {kontakt}</div>}
     </div>
-  )
+  );
 }
 
-export default Meist
+export default Meist;
