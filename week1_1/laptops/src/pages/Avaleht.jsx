185aa432284ecd0fb4314a3b3e76b1ae6c2ea19b
Mark Hallap <hallapmark@gmail.com>
2025-10-26 23:56:43 +0200

    hw week 1_1 laptops

---
+++ week1_1/laptops/src/pages/Avaleht.jsx
@@ -0,0 +1,10 @@
+function Avaleht() {
+  return (
+    <div className ="avaleht-text">
+      <div>Tere</div>
+      <div>Siin lehel saad sülearvuteid vaadata ja lisada.</div>
+    </div>
+  )
+}
+
+export default Avaleht
--- week1_1/uudised/src/App.jsx
+++ week1_1/uudised/src/App.jsx
@@ -11,24 +11,7 @@ function App() {
 
   return (
     <>
-      <Link to="/">
-        <button>Avalehele</button>
-      </Link>
-      <Link to="/uudised">
-        <button>Uudiste lehele</button>
-      </Link>
-      <Link to="/kontakt">
-        <button>Võta meiega ühendust</button>
-      </Link>
-      <Link to="/meist">
-        <button>Info meist</button>
-      </Link>
-      <Routes>
-        <Route path='' element={ <Avaleht /> } />
-        <Route path='uudised' element={ <Uudised /> } />
-        <Route path='kontakt' element={ <Kontakt /> } />
-        <Route path='meist' element={ <Meist /> } />
-      </Routes>
+      
     </>
   )
 }
